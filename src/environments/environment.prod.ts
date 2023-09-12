ae546d9fb0133943a9bba2f0304f9e9b78686434
dmedinam <dmedinam@ri.gob.do>
2023-09-12 16:06:30 -0400

    add max budget warning add env on prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  jwtSecret: 'KLMLK45M34KLMSLKM3L5KMS554653452344212SF45SDF3',
+  apiUrl: 'https://119.8.0.10:4543/api',
+  recaptcha: {
+    siteKey: '6LdtVBEoAAAAACFjnKUI39ysO7gzKA2BCDoQ3nAa',
+  },
 };
